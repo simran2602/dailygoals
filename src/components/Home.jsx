efa4467b6371e04a55b031cd182e6c64e3408613
shiulimondal <shiulimondal927@gmail.com>
2024-04-15 01:06:35 +0530

    up4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Task from './Task'
 import { useState } from 'react'
+import Report from './Report'
 
 const Home = () => {
     const intialArray = localStorage.getItem("task")
@@ -9,6 +10,7 @@ const Home = () => {
     const [task, setTask] = useState(intialArray);
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const[taskDone,setTaskDone]= useState([])
 
     const titleHandler = (e) => {
         setTitle(e.target.value)
@@ -16,6 +18,7 @@ const Home = () => {
     const descriptionHandler = (e) => {
         setDescription(e.target.value)
     }
+    const titleRef = useRef(null)
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -26,6 +29,7 @@ const Home = () => {
         setTask([...task, newTodo]);
         setTitle("");
         setDescription("");
+        titleRef.current.focus();
     }
 
     const handleKeyDown = (e) => {
@@ -33,34 +37,46 @@ const Home = () => {
             submitHandler(e);
         }
     };
-    
+
     useEffect(() => {
         localStorage.setItem("task", JSON.stringify(task))
     }, [task])
 
+    // const CompletedWorkArray = []
     const deleteTask = (index) => {
         const filteredArray = task.filter((val, i) => {
+            if(i==index){
+                setTaskDone(prevTaskDone => [...prevTaskDone, task[i]]);
+                // setTaskDone([...taskDone,task[i]])
+            }           
             return i !== index
         })
         console.log("filteredArray", filteredArray);
+        // console.log("completed",taskDone);
+
         setTask(filteredArray)
     }
-    console.log("task", task);
+    useEffect(() => {
+        console.log("completed", taskDone);
+    }, [taskDone]);
+    // console.log("task", task);
     // console.log(...task);
     return (
         <div className='container'>
             <h1>Daily Goals</h1>
             <form onSubmit={submitHandler}>
-                <input type='text' placeholder='Title' onChange={titleHandler} value={title} />
+                <input type='text' placeholder='Title' onChange={titleHandler} value={title} ref = {titleRef}/>
                 <textarea placeholder='Description' onChange={descriptionHandler} value={description} onKeyDown={handleKeyDown} ></textarea>
                 <button type="submit">Add</button>
             </form>
+            <Report/>
             {task.map((item, index) => (
                 <div key={index}>
                     <Task title={item.title} description={item.description}
                         deleteTask={deleteTask} index={index} />
                 </div>
             ))}
+          
 
 
 
